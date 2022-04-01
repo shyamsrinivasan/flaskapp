08a2a5cf4c37ea874390b6ce71585afb1a8d1f7e
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-01 09:17:42 +0530

    change to template format suitable for Flask render_template

--- app/script.py
+++ app/script.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 from app import app
 
 
-@app.route('/home')
+@app.route('/')
+@app.route('/index.html')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+
+@app.route('/admin/index.html')
+def admin_home():
+    return render_template('/admin/index.html')
+
+
+@app.route('/admin/client/index.html')
+def client_home():
+    return render_template('/admin/client/index.html')
+
+
+@app.route('/office/index.html')
+def office_home():
+    return render_template('/admin/office/index.html')
+
+
+@app.route('/taxes/index.html')
+def taxes_home():
+    return render_template('/taxes/index.html')
 
 
 if __name__ == '__main__':
