af8a056bb05d8fbed6d9352f533e939ffb8042b6
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-03 13:49:29 +0530

    add all routes from example-2

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,53 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
-    user = {'username': 'User'}
-    posts = [
-        {'author': {'username': 'John'}, 'body': 'Beautiful day in New York'},
-        {'author': {'username': 'Smith'}, 'body': 'Cool comic book movie'}
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/index.html', methods=['GET', 'POST'])
+def login_home():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return render_template('/login/loginform.html', user=username)
+    else:
+        return render_template('/login/index.html')
+
+
+@app.route('/admin/index.html')
+def admin_home():
+    return render_template('/admin/index.html')
+
+
+@app.route('/admin/client/index.html')
+def client_home():
+    return render_template('/admin/client/index.html')
+
+
+@app.route('/office/index.html')
+def office_home():
+    return render_template('/admin/office/index.html')
+
+
+@app.route('/taxes/index.html')
+def taxes_home():
+    return render_template('/taxes/index.html')
+
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     user = {'username': 'User'}
+#     posts = [
+#         {'author': {'username': 'John'}, 'body': 'Beautiful day in New York'},
+#         {'author': {'username': 'Smith'}, 'body': 'Cool comic book movie'}
+#     ]
+#     return render_template('index.html', title='Home', user=user, posts=posts)
     # '''
     # <html>
     #     <head>
