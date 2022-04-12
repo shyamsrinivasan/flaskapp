c7b7478b98a0f77ccbaaa19171f00960f109fcd7
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-12 22:31:55 +0530

    declare blueprint for use in app.__init__

--- app/home/views.py
+++ app/home/views.py
@@ -2,21 +2,21 @@ from flask import Blueprint, render_template
 # from flask_bs4 import Bootstrap
 
 
-home = Blueprint('home', __name__)
+home = Blueprint('home', __name__, template_folder='templates')
 
 
 @home.route('/')
 @home.route('/index.html')
 def index():
-    return render_template('/home/index.html')
+    return render_template('/index.html')
 
 
 @home.route('/login.html')
 def admin_home():
-    return render_template('/home/login.html')
+    return render_template('/login.html')
 
 
 @home.route('/signup.html')
 def signup_home():
-    return render_template('/home/signup.html')
+    return render_template('/signup.html')
 
