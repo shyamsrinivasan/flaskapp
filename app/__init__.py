3387b43d7e94418c39203872200bbc34ac5b38bd
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-12 22:30:45 +0530

    use blueprints and register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,34 @@
 from flask import Flask
-app = Flask(__name__)
-from app import routes
+# from flask_bs4 import Bootstrap
+# from .home import home
+# from .views.admin import admin
+# from .views.taxes import taxes
+import os
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    # configure application
+    # app.config.from_mapping(SECRET_KEY='dev')
+    app.config.from_object('config.TestConfig')
+    # app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+    # Bootstrap(app)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    with app.app_context():
+        from .home import views
+
+        # registering blueprint with app
+        app.register_blueprint(views.home)
+        # app.register_blueprint(admin)
+
+    return app
+
+    # from app import routes
