eb5dd6300487320e0981ab8b09c5d83fd94acce1
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-14 00:31:33 +0530

    remove .html in endpoints in route decorators

--- app/home/views.py
+++ app/home/views.py
@@ -3,12 +3,17 @@ from . import home_bp
 
 
 @home_bp.route('/')
-@home_bp.route('/index.html')
+@home_bp.route('/index')
 def index():
     return render_template('/index.html')
 
 
-@home_bp.route('/login.html', methods=['GET', 'POST'])
+@home_bp.route('/about')
+def about():
+    return render_template('/about.html')
+
+
+@home_bp.route('/login', methods=['GET', 'POST'])
 def login_home():
     if request.method == 'POST':
         username = request.form['username']
@@ -18,11 +23,12 @@ def login_home():
         return render_template('/login.html')
 
 
-@home_bp.route('/signup.html', methods=['GET', 'POST'])
+@home_bp.route('/signup', methods=['GET', 'POST'])
 def signup_home():
     if request.method == 'POST':
         # process sign-up information using func into db
         return None
     else:
-        return render_template('/signup.html')
+        # temporary render template to test usage of WTF-Forms
+        return render_template('/contact.html')
 
