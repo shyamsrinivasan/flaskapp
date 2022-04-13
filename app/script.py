77959065dc1b262e06706a01225a4fd349bc7477
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-13 16:52:20 +0530

    example for using GET and POST methods with FLASK and Jinja

--- app/script.py
+++ app/script.py
@@ -28,5 +28,39 @@ def taxes_home():
     return render_template('/taxes/index.html')
 
 
+@app.route('/login/index.html')
+def login_home():
+    return render_template('/login/index.html')
+
+
+# @app.route()
+def login_form():
+    return render_template('/login/loginform.html')
+
+
+# example forms using Flask and HTML
+"""from flask import render_template, url_for, request, redirect
+    app = Flask(__name__)
+    
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+        
+    
+    @app.route('/form', methods = ['GET', 'POST'])
+    def form():
+        if request.method == 'POST':
+            glon = request.form['glon']
+            return redirect(url_for('display.html', glon=glon))
+            
+        return render_template('form.html')
+        
+    
+    @app.route('/return_form/<glon>')
+    def return_form(glon):
+        return render_template('return_form.html', glon=glon)"""
+
+
 if __name__ == '__main__':
     app.run(debug=True)
