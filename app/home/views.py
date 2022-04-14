92b8d6d9a543b626e0f850305f34da533aeb0a89
shyamsrinivasan <shyamsweth@gmail.com>
2022-04-14 11:42:53 +0530

    use only object based config

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from . import home_bp
+from .forms import ContactForm
 
 
 @home_bp.route('/')
@@ -23,11 +24,12 @@ def login_home():
         return render_template('/login.html')
 
 
-@home_bp.route('/signup', methods=['GET', 'POST'])
+@home_bp.route('/contact', methods=['GET', 'POST'])
 def signup_home():
-    if request.method == 'POST':
+    form = ContactForm()
+    if form.validate_on_submit():
         # process sign-up information using func into db
-        return None
+        return redirect(url_for("success"))
     else:
         # temporary render template to test usage of WTF-Forms
         return render_template('/contact.html')
