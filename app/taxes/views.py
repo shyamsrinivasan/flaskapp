26835d8db02e7928142616353be693c01ac138e1
Shyam Srinivasan <shyamsweth@gmail.com>
2022-09-23 19:09:50 +0530

    view routed to new taxes.html

--- app/taxes/views.py
+++ app/taxes/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import render_template
 from . import taxes_bp
 
 
 @taxes_bp.route('/taxes')
-@taxes_bp.route('/taxes/index.html')
+# @taxes_bp.route('/taxes/index.html')
 def taxes_home():
-    return render_template('/index.html')
+    return render_template('taxes.html')
